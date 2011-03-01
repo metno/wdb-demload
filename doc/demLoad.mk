c65969a52b83e34355a7967fffbcc7cf5dfdbfd1
Michael Akinde <michael.akinde@met.no>
2011-03-01 21:54:35 +0000

    Removed installation of users in demLoad.

--- doc/demLoad.mk
+++ doc/demLoad.mk
@@ -3,9 +3,9 @@ man_MANS = demLoad.1
 demLoad.1:	doc/demLoad.man.xml doc/docbook/wdb_entities.ent
 	$(DOCBOOK) man $<
 	
-EXTRA_DIST = doc/demLoad.man.xml \
+EXTRA_DIST += doc/demLoad.man.xml \
 	doc/docbook/wdb_entities.ent \
 	doc/docbook/xinclude.mod
 	
-CLEANFILES = demLoad.1
+CLEANFILES += demLoad.1
 
